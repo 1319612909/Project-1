b89b2af3a10fdaab575e049ca0a55b38d2fd0c04
jinlan.du <jinlan.du@cyberplus.com>
2020-04-07 13:37:55 +0800

    测试

---
+++ src/store/modules/route.js
@@ -0,0 +1,23 @@
+// import router from '@/router'
+import { routes } from '@/router/index'
+
+const app = {
+  state: {
+    routes: routes,
+  },
+  mutations: {
+    setroutes: (state, routes) => {
+      if (state.lock) return
+    },
+    routerFlag: (state, flag) => {
+      state.routerFlag = flag
+    }
+  },
+  actions: {
+  
+  }
+}
+
+export default app
+
+
---
+++ src/views/test.vue
@@ -0,0 +1,53 @@
+<template>
+ <div class="warp">
+    <form  method="post">
+      <input @change="fileChange" id="file" multiple="multiple" type="file" />
+    </form>
+    <!-- <input type="file" @change="fileChange($event)"> -->
+    <div class="list">
+      <transition-group name="list" tag="p">
+        <span v-for="item in list" v-bind:key="item" class="list-item">
+          {{ item.name }}
+        </span>
+      </transition-group>
+   </div>
+
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+      list:[],
+      file:null,
+     }
+   },
+   methods:{
+     fileChange(e){
+        this.file = e.target.files[0]
+        this.list.push(this.file)
+     }
+   }
+ }
+</script>
+
+<style>
+.warp{
+  width: 50%;
+}
+
+.list-item {
+  display: block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to
+/* .list-leave-active for below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+ 
+</style>
