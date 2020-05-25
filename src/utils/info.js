aa5d64cdd1e03310ef3fb94fe666d1a94ed71cd2
1319612909 <1319612909@qq.com>
2020-05-25 14:32:30 +0800

    1

--- src/utils/info.js
+++ src/utils/info.js
@@ -11,3 +11,39 @@ export function contractShow(item){
         return false
     }
 }
+
+
+
+   /*获取某年某月的总天数*/
+   export function  getDaysOfMonth(year, month) {
+    var date = new Date(year, month, 0);
+    var days = date.getDate();
+    return days;
+  }
+
+/*获取当前月的总天数*/
+export function getDays() {
+var date = new Date();
+//将当前月份加1，下移到下一个月
+date.setMonth(date.getMonth() + 1);
+//将当前的日期置为0，
+date.setDate(0);
+//再获取天数即取上个月的最后一天的天数
+var days = date.getDate();
+return days;
+}
+
+export function getD(starDate, endDate,day,m) {
+//参数格式  starDate ： 2017-07-08 2017/01/12， endDate： 2017-07-08 2017/01/12  day: 2020, m:05/5
+var sDate = new Date(starDate).getTime();
+var eDate = new Date(endDate).getTime();
+// var thisMothDays = 1000 * 3600 * 24 * getDays()
+var thisMothDays = 1000 * 3600 * 24 * this.getDaysOfMonth(day,m)
+if (eDate - sDate > thisMothDays) {
+console.log('超过一个月')
+    return true
+} else {
+console.log('未超过一个月')
+    return false
+}
+}
