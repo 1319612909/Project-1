75e8357794712b9841ff83e818c4dfff865979c6
jinlan.du <jinlan.du@cyberplus.com>
2020-06-03 15:01:53 +0800

    add

--- src/utils/info.js
+++ src/utils/info.js
@@ -12,8 +12,6 @@ export function contractShow(item){
     }
 }
 
-
-
    /*获取某年某月的总天数*/
    export function  getDaysOfMonth(year, month) {
     var date = new Date(year, month, 0);
@@ -46,4 +44,44 @@ console.log('超过一个月')
 console.log('未超过一个月')
     return false
 }
+}
+
+export function download(){
+    // const resFile = await  downloadReconciliation(fileDate)  // 后台请求回来的boba流
+    if(res.headers.success === "true"){
+        let fileName = "运营统计.xls"
+        const blob = new Blob([res.data])
+        if (window.navigator.msSaveOrOpenBlob) {
+            navigator.msSaveBlob(blob, fileName)
+        } else {
+            var link = document.createElement('a')
+            link.href = window.URL.createObjectURL(blob)
+            link.download = fileName
+            link.click()
+            window.URL.revokeObjectURL(link.href)
+        }
+    }else{
+        const temp = res.headers.errormessage
+        this.$message.error(decodeURIComponent(temp));
+        return
+    }
+}
+// 去重 + 排序
+export function sortarr(arr){
+	 var newArr = []
+            for(var i=0;i<arr.length;i++){
+               if(newArr.indexOf(arr[i])<0){
+                   newArr.push(arr[i])
+               }
+            }
+            for(i=0;i<newArr.length-1;i++){
+                for(j=0;j<newArr.length-1-i;j++){
+                    if(newArr[j]>newArr[j+1]){
+                    var temp=newArr[j];
+                    newArr[j]=newArr[j+1];
+                    newArr[j+1]=temp;
+                    }
+                }
+            }
+        return newArr;
 }
